4e5219d7d8430956699338e5229f88aceff42838
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2009-03-23 19:00:48 +0000

    Starting a validation example

---
+++ trunk/hapi-mvn/hapi-examples/src/main/java/ca/uhn/hl7v2/examples/MessageValidation.java
@@ -0,0 +1,28 @@
+/*
+ * Created on Mar 23, 2009
+ */
+package ca.uhn.hl7v2.examples;
+
+/**
+ * TODO: add!
+ * 
+ * @author <a href="mailto:jamesagnew@sourceforge.net">James Agnew</a>
+ * @version $Revision: 1.1 $ updated on $Date: 2009-03-23 19:00:48 $ by $Author: jamesagnew $
+ */
+public class MessageValidation
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /*
+         * MSH|^~\&|MedSeries|CAISI_1-2|PLS|3910|200903230934||ADT^A31^ADT_A05|75535037-1237815294895|P^T|2.4
+         * EVN|A31|200903230934
+         * PID|1||29^^CAISI_1-2^PI~""||Test300^Leticia^^^^^L||19770202|M||||||||||||||||||||||
+         */
+        
+    }
+
+}
