0c8efd63a168dbcb9899d56d523a7511168c5fbb
James Agnew <jamesagnew@gmail.com>
2009-08-07 22:40:20 +0000

    New example

---
+++ hapi-examples/src/main/java/ca/uhn/hl7v2/examples/CorrectInvalidMessageBeforeParsing.java
@@ -0,0 +1,97 @@
+/**
+The contents of this file are subject to the Mozilla Public License Version 1.1 
+(the "License"); you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at http://www.mozilla.org/MPL/ 
+Software distributed under the License is distributed on an "AS IS" basis, 
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the 
+specific language governing rights and limitations under the License. 
+
+The Original Code is "CorrectInvalidMessageBeforeParsing.java". 
+
+The Initial Developer of the Original Code is University Health Network. Copyright (C) 
+2001.  All Rights Reserved. 
+
+Contributor(s): ______________________________________. 
+
+Alternatively, the contents of this file may be used under the terms of the 
+GNU General Public License (the  �GPL�), in which case the provisions of the GPL are 
+applicable instead of those above.  If you wish to allow use of your version of this 
+file only under the terms of the GPL and not to allow others to use your version 
+of this file under the MPL, indicate your decision by deleting  the provisions above 
+and replace  them with the notice and other provisions required by the GPL License.  
+If you do not delete the provisions above, a recipient may use your version of 
+this file under either the MPL or the GPL. 
+
+*/
+
+package ca.uhn.hl7v2.examples;
+
+import sun.awt.windows.ThemeReader;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.EncodingNotSupportedException;
+import ca.uhn.hl7v2.parser.Parser;
+import ca.uhn.hl7v2.parser.PipeParser;
+
+/**
+ * Example
+ * 
+ * @author <a href="mailto:jamesagnew@sourceforge.net">James Agnew</a>
+ * @version $Revision: 1.1 $ updated on $Date: 2009-08-07 22:40:20 $ by $Author: jamesagnew $
+ */
+public class CorrectInvalidMessageBeforeParsing
+{
+
+    /**
+     * @param args
+     * @throws HL7Exception 
+     */
+    public static void main(String[] args) throws HL7Exception {
+
+        /*
+         * In an ideal world, systems would always send perfect messages, but in the real world, systems
+         * will always exist that send crazy data. HAPI tries to be tolerant of this, but occasionally
+         * a system will come along that sends something so wacky that HAPI is unable to parse it.
+         * 
+         * One possible solution to this is to manually fix such problems before parsing them. One
+         * easy way to do that is with a subclass of your parser.
+         */
+        
+        // This message has PI instead of PID in the second line:
+        String msg = "MSH|^~\\&|HIS|RIH|EKG|EKG|199904140038||ADT^A01||P|2.2\r"
+            + "PI|0001|00009874|00001122|A00977|SMITH^JOHN^M|MOM|19581119|F|NOTREAL^LINDA^M|C|564 SPRING ST^^NEEDHAM^MA^02494^US|0002|(818)565-1551|(425)828-3344|E|S|C|0000444444|252-00-4414||||SA|||SA||||NONE|V1|0001|I|D.ER^50A^M110^01|ER|P00055|11B^M011^02|070615^BATMAN^GEORGE^L|555888^NOTREAL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^NOTREAL^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|199904101200||||5555112333|||666097^NOTREAL^MANNY^P\r"
+            + "NK1|0222555|NOTREAL^JAMES^R|FA|STREET^OTHER STREET^CITY^ST^55566|(222)111-3333|(888)999-0000|||||||ORGANIZATION\r"
+            + "PV1|0001|I|D.ER^1F^M950^01|ER|P000998|11B^M011^02|070615^BATMAN^GEORGE^L|555888^OKNEL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^VOICE^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|||||5555112333|||666097^DNOTREAL^MANNY^P\r"
+            + "PV2|||0112^TESTING|55555^PATIENT IS NORMAL|NONE|||19990225|19990226|1|1|TESTING|555888^NOTREAL^BOB^K^DR^MD||||||||||PROD^003^099|02|ER||NONE|19990225|19990223|19990316|NONE\r"
+            + "AL1||SEV|001^POLLEN\r"
+            + "GT1||0222PL|NOTREAL^BOB^B||STREET^OTHER STREET^CITY^ST^77787|(444)999-3333|(222)777-5555||||MO|111-33-5555||||NOTREAL GILL N|STREET^OTHER STREET^CITY^ST^99999|(111)222-3333\r"
+            + "IN1||022254P|4558PD|BLUE CROSS|STREET^OTHER STREET^CITY^ST^00990||(333)333-6666||221K|LENIX|||19980515|19990515|||PATIENT01 TEST D||||||||||||||||||02LL|022LP554";
+
+        // Let's create a special parser to handle this:
+        Parser parser = new PipeParser() {
+
+            // We override the parse method to correct issues
+            public Message parse(String theMessage) throws HL7Exception, EncodingNotSupportedException {
+                theMessage = theMessage.replace("\rPI|", "\rPID|");
+                return super.parse(theMessage);
+            }};
+        
+        Message message = parser.parse(msg);
+        
+        System.out.println(parser.encode(message));
+        
+        /* PI has been fixed:
+         
+            MSH|^~\&|HIS|RIH|EKG|EKG|199904140038||ADT^A01||P|2.2
+            PID|0001|00009874|00001122|A00977|SMITH^JOHN^M|MOM|19581119|F|NOTREAL^LINDA^M|C|564 SPRING ST^^NEEDHAM^MA^02494^US|0002|(818)565-1551|(425)828-3344|E|S|C|0000444444|252-00-4414||||SA|||SA||||NONE|V1|0001|I|D.ER^50A^M110^01|ER|P00055|11B^M011^02|070615^BATMAN^GEORGE^L|555888^NOTREAL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^NOTREAL^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|199904101200||||5555112333|||666097^NOTREAL^MANNY^P
+            NK1|0222555|NOTREAL^JAMES^R|FA|STREET^OTHER STREET^CITY^ST^55566|(222)111-3333|(888)999-0000|||||||ORGANIZATION
+            PV1|0001|I|D.ER^1F^M950^01|ER|P000998|11B^M011^02|070615^BATMAN^GEORGE^L|555888^OKNEL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^VOICE^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|||||5555112333|||666097^DNOTREAL^MANNY^P
+            PV2|||0112^TESTING|55555^PATIENT IS NORMAL|NONE|||19990225|19990226|1|1|TESTING|555888^NOTREAL^BOB^K^DR^MD||||||||||PROD^003^099|02|ER||NONE|19990225|19990223|19990316|NONE
+            AL1||SEV|001^POLLEN
+            GT1||0222PL|NOTREAL^BOB^B||STREET^OTHER STREET^CITY^ST^77787|(444)999-3333|(222)777-5555||||MO|111-33-5555||||NOTREAL GILL N|STREET^OTHER STREET^CITY^ST^99999|(111)222-3333
+            IN1||022254P|4558PD|BLUE CROSS|STREET^OTHER STREET^CITY^ST^00990||(333)333-6666||221K|LENIX|||19980515|19990515|||PATIENT01 TEST D||||||||||||||||||02LL|022LP554
+         */
+        
+    }
+
+}
