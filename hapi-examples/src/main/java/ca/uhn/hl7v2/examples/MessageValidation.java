db74abc6044d9ad7fc8b05f7158cda49937e5fa5
James Agnew <jamesagnew@gmail.com>
2009-08-09 13:59:46 +0000

    Add license block

--- hapi-examples/src/main/java/ca/uhn/hl7v2/examples/MessageValidation.java
+++ hapi-examples/src/main/java/ca/uhn/hl7v2/examples/MessageValidation.java
@@ -1,5 +1,28 @@
-/*
- * Created on Mar 23, 2009
+/**
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+ * specific language governing rights and limitations under the License.
+ *
+ * The Original Code is "MessageValidation.java".  Description:
+ * "Example Code"
+ *
+ * The Initial Developer of the Original Code is University Health Network. Copyright (C)
+ * 2001.  All Rights Reserved.
+ *
+ * Contributor(s): James Agnew
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * GNU General Public License (the  �GPL�), in which case the provisions of the GPL are
+ * applicable instead of those above.  If you wish to allow use of your version of this
+ * file only under the terms of the GPL and not to allow others to use your version
+ * of this file under the MPL, indicate your decision by deleting  the provisions above
+ * and replace  them with the notice and other provisions required by the GPL License.
+ * If you do not delete the provisions above, a recipient may use your version of
+ * this file under either the MPL or the GPL.
+ *
  */
 package ca.uhn.hl7v2.examples;
 
@@ -15,7 +38,7 @@ import ca.uhn.hl7v2.validation.impl.ValidationContextImpl;
  * TODO: add!
  * 
  * @author <a href="mailto:jamesagnew@sourceforge.net">James Agnew</a>
- * @version $Revision: 1.2 $ updated on $Date: 2009-03-27 01:13:23 $ by $Author:
+ * @version $Revision: 1.3 $ updated on $Date: 2009-08-09 13:59:46 $ by $Author:
  *          jamesagnew $
  */
 public class MessageValidation {
