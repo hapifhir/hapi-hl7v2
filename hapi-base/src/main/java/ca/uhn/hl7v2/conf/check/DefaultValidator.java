0a85363d8666727c4ab7ee583f2c80d0c531d5ae
James Agnew <jamesagnew@gmail.com>
2009-08-19 01:45:29 +0000

    Fix bug 1809573

--- hapi-base/src/main/java/ca/uhn/hl7v2/conf/check/DefaultValidator.java
+++ hapi-base/src/main/java/ca/uhn/hl7v2/conf/check/DefaultValidator.java
@@ -15,7 +15,7 @@ The Initial Developer of the Original Code is University Health Network. Copyrig
 Contributor(s): ______________________________________. 
 
 Alternatively, the contents of this file may be used under the terms of the 
-GNU General Public License (the  “GPL”), in which case the provisions of the GPL are 
+GNU General Public License (the  ï¿½GPLï¿½), in which case the provisions of the GPL are 
 applicable instead of those above.  If you wish to allow use of your version of this 
 file only under the terms of the GPL and not to allow others to use your version 
 of this file under the MPL, indicate your decision by deleting  the provisions above 
@@ -387,7 +387,7 @@ public class DefaultValidator implements Validator {
      */
     private HL7Exception[] testTypeAgainstTable(Type type, AbstractComponent profile, String profileID) {
         ArrayList exList = new ArrayList();
-        if (profile.getTable() != null && type.getName().equals("IS") || type.getName().equals("ID")) {
+        if (profile.getTable() != null && (type.getName().equals("IS") || type.getName().equals("ID"))) {
             String codeSystem = makeTableName(profile.getTable());
             String value = ((Primitive) type).getValue();
             addTableTestResult(exList, profileID, codeSystem, value);
