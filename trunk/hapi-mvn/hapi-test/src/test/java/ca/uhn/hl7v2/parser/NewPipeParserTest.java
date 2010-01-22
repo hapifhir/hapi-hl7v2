eb63210b388b37eca68d90e45b60ba8a989003aa
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2010-01-22 01:00:49 +0000

    -

--- trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/parser/NewPipeParserTest.java
+++ trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/parser/NewPipeParserTest.java
@@ -97,12 +97,14 @@ public class NewPipeParserTest extends TestCase {
 				+ "OBR|0||T09-100442-RET-0^^OLIS_Site_ID^ISO|RET^RETICULOCYTE COUNT^HL79901 literal|||200905011106|||||||200905011106||OLIST^BLAKE^DONALD^THOR^^^^L^921379||7870279|7870279|T09-100442|MOHLTC|200905011130||B7|F||1^^^200905011106^^R\r"
 				+ "OBX|1|NM|Z114099^Erc^L||4.00|tril/L|3.90-5.60||||F|||200905011111|PMH\r";
 
-		Message message = parser.parse(string);
+		ca.uhn.hl7v2.model.v23.message.ORU_R01 message = (ca.uhn.hl7v2.model.v23.message.ORU_R01) parser.parse(string);
 
 		String reEncoded = parser.encode(message);
 		System.out.println(reEncoded);
 		assertEquals(string, reEncoded);
 
+		String pidMrn = message.getRESPONSE().getPATIENT().getPID().getPid3_PatientIDInternalID(0).getCx1_ID().getValue();
+		assertEquals("7005728", pidMrn);
 	}
 
 	public void testNonStandardSegmentNearEnd() throws EncodingNotSupportedException, HL7Exception {
