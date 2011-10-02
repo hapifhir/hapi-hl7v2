ac77e2d5bd4241eac8020cd0f384c40fd53525df
James Agnew <jamesagnew@gmail.com>
2011-10-02 15:43:11 +0000

    Add forced encode mode

--- hapi-base/src/main/java/ca/uhn/hl7v2/parser/ParserConfiguration.java
+++ hapi-base/src/main/java/ca/uhn/hl7v2/parser/ParserConfiguration.java
@@ -1,37 +1,75 @@
 package ca.uhn.hl7v2.parser;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import ca.uhn.hl7v2.util.Terser;
+
 
 public class ParserConfiguration {
 
-    private boolean myEncodeEmptySegments = false;
-
-    /**
-     * Returns the value returned by {@link #isEncodeEmptySegments()}, default is <code>false</code> 
-     * 
-     * @see #isEncodeEmptySegments()
-     */
-	public boolean isEncodeEmptySegments() {
-		return myEncodeEmptySegments;
-	}
-
-
+    private Set<String> myForcedEncode = new HashSet<String>();
+    
+	
     /**
      * <p>
-     * If set to <code>true</code> (default is <code>false</code>), segments will be encoded
-     * even if they are empty (i.e. if the corresponding getXXX() method has been called at
-     * least once but no content has been added to the segment). Note that this doesn't mean
-     * that all segments in the message will automatically be encoded, you will need to
-     * call the getXXX method at least once on any segments you wish to have encoded.
+     * Forces the parser to encode certain segments/fields, even if they contain no content. This
+     * method may be called multiple times with multiple path definitions, and each path definition
+     * contains the path to the segment or field which needs to be forced.
      * </p>
+     * <p>
+     * Path definitions are similar in format to {@link Terser Terser} paths. They contain
+     * a slash-separated lookup path to reach a given segment, and optionally a field number. The
+     * following are examples of paths which could be added here, as well as the sample output for 
+     * an otherwise empty ORU^R01 message:
+     * </p>
+     * <table>
+     *    <th>
+     *       <td>Forced Encode Path</td>
+     *       <td>Encode Output</td>
+     *    </th>
+     *    <tr>
+     *       <td>PATIENT_RESULT/ORDER_OBSERVATION/ORC</td>
+     *       <td>MSH|^~\&amp;|||||||ORU^R01^ORU_R01||T|2.4<br>ORC|</td>
+     *    </tr>
+     *    <tr>
+     *       <td>PATIENT_RESULT/ORDER_OBSERVATION/ORC-4</td>
+     *       <td>MSH|^~\&amp;|||||||ORU^R01^ORU_R01||T|2.4<br>ORC||||</td>
+     *    </tr>
+     * </table>
      * <p>
      * While empty segments do not generally have any meaning according
      * to HL7, this may be useful when transmitting to systems which rely on segments
      * being received even if they have no content.
      * </p>
+     * 
+     * @since 1.3
      */
-	public void setEncodeEmptySegments(boolean theEncodeEmptySegments) {
-		myEncodeEmptySegments = theEncodeEmptySegments;
+	public void addForcedEncode(String theForcedEncode) {
+		if (theForcedEncode == null) {
+			throw new NullPointerException("forced encode may not be null");
+		}
+		
+		int lastSlashIndex = theForcedEncode.lastIndexOf('/');
+		lastSlashIndex = Math.max(lastSlashIndex, 0);
+		
+		if (lastSlashIndex == theForcedEncode.length() || !theForcedEncode.substring(lastSlashIndex + 1).matches("[A-Z]{3}(-[0-9]+){0,1}$")) {
+			throw new IllegalArgumentException("Definition must end with a segment name or field lookup, e.g. MSH or MSH-2");
+		}
+		
+		myForcedEncode.add(theForcedEncode);
 	}
-    
+
+
+	/**
+	 * @return Returns the forced encode strings added by {@link #addForcedEncode(String)}
+	 * 
+	 * @see #addForcedEncode(String)
+	 * @since 1.3
+	 */
+	public Set<String> getForcedEncode() {
+		return myForcedEncode;
+	}
+	
 	
 }
