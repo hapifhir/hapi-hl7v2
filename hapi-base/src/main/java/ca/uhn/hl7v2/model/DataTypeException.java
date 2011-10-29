81e826c5d7abcaffd636cc7e86fb91a1ec1e630e
James Agnew <jamesagnew@gmail.com>
2011-10-29 17:22:59 +0000

    Fixing up non UTF-8 character in license header in lots of files

--- hapi-base/src/main/java/ca/uhn/hl7v2/model/DataTypeException.java
+++ hapi-base/src/main/java/ca/uhn/hl7v2/model/DataTypeException.java
@@ -16,7 +16,7 @@ The Initial Developer of the Original Code is University Health Network. Copyrig
 Contributor(s): ______________________________________. 
 
 Alternatively, the contents of this file may be used under the terms of the 
-GNU General Public License (the  “GPL”), in which case the provisions of the GPL are 
+GNU General Public License (the "GPL"), in which case the provisions of the GPL are 
 applicable instead of those above.  If you wish to allow use of your version of this 
 file only under the terms of the GPL and not to allow others to use your version 
 of this file under the MPL, indicate your decision by deleting  the provisions above 
