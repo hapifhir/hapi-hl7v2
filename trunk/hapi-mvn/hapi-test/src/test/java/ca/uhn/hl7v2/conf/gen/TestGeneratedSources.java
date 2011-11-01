edd376c37537baa3cba3c5cccee0dcdd7997df96
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2011-11-01 13:10:24 +0000

    Add test case

--- trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/conf/gen/TestGeneratedSources.java
+++ trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/conf/gen/TestGeneratedSources.java
@@ -1,5 +1,12 @@
 package ca.uhn.hl7v2.conf.gen;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.test.singledt.conf.message.ADT_A01;
+import ca.uhn.hl7v2.test.singledt.conf.segment.OBX;
 import ca.uhn.hl7v2.test.tblnms.conf.message.ACK;
 import ca.uhn.hl7v2.test.tblnms.conf.segment.MSH;
 import junit.framework.TestCase;
@@ -14,6 +21,22 @@ public class TestGeneratedSources extends TestCase {
 		assertEquals("IHE", msh.getAcceptAcknowledgmentType().getTableNamespace());
 		
 		assertEquals(155, msh.getApplicationAcknowledgmentType().getTable());
+		
+	}
+	
+	
+	public void testGetAllReps() throws HL7Exception, IOException {
+
+		ADT_A01 msg = new ADT_A01();
+		msg.getOBX(0).getSetIDOBX().setValue("1");
+		msg.getOBX(1).getSetIDOBX().setValue("2");
+		
+		List<OBX> all = msg.getOBXAll();
+		Iterator<OBX> iter = all.iterator();
+		
+		assertEquals("1", iter.next().getSetIDOBX().getValue());
+		assertEquals("2", iter.next().getSetIDOBX().getValue());
+		assertFalse(iter.hasNext());
 		
 	}
 	
