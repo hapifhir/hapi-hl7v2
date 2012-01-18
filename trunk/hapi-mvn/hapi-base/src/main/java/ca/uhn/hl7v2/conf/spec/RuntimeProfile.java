f77725a9fa78e58466decedffd38e10df1b6f392
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-01-18 13:36:04 +0000

    Load metadata from Conformance Profiles

--- trunk/hapi-mvn/hapi-base/src/main/java/ca/uhn/hl7v2/conf/spec/RuntimeProfile.java
+++ trunk/hapi-mvn/hapi-base/src/main/java/ca/uhn/hl7v2/conf/spec/RuntimeProfile.java
@@ -10,44 +10,45 @@ import ca.uhn.hl7v2.conf.ProfileException;
  */
 public class RuntimeProfile {
     
-    private String impNote;
-    private UseCase useCase;
     private Conformance conformance;
+    /** Holds value of property conformanceType. */
+    private String conformanceType;
     private Encoding[] encodings;
-    private StaticDef StaticDef;
+    /** Holds value of property HL7OID. */
+    private String HL7OID;
+    /** Holds value of property HL7Version. */
+    private String HL7Version;
         
-    /** Utility field used by bound properties. */
-    private java.beans.PropertyChangeSupport propertyChangeSupport =  new java.beans.PropertyChangeSupport(this);
-    
-    /** Utility field used by constrained properties. */
-    private java.beans.VetoableChangeSupport vetoableChangeSupport =  new java.beans.VetoableChangeSupport(this);
+    private String impNote;
     
     /** Holds value of property message. */
     private StaticDef message;
     
-    /** Holds value of property specName. */
-    private String specName;
-    
     /** Holds value of property orgName. */
     private String orgName;
     
-    /** Holds value of property HL7Version. */
-    private String HL7Version;
-    
-    /** Holds value of property specVersion. */
-    private String specVersion;
-    
-    /** Holds value of property status. */
-    private String status;
+    /** Utility field used by bound properties. */
+    private java.beans.PropertyChangeSupport propertyChangeSupport =  new java.beans.PropertyChangeSupport(this);
     
     /** Holds value of property role. */
     private String role;
     
-    /** Holds value of property HL7OID. */
-    private String HL7OID;
+    /** Holds value of property specName. */
+    private String specName;
     
-    /** Holds value of property conformanceType. */
-    private String conformanceType;
+    /** Holds value of property specVersion. */
+    private String specVersion;
+    
+    private StaticDef StaticDef;
+    
+    /** Holds value of property status. */
+    private String status;
+    
+    private UseCase useCase;
+    
+    /** Utility field used by constrained properties. */
+    private java.beans.VetoableChangeSupport vetoableChangeSupport =  new java.beans.VetoableChangeSupport(this);
+	private String name;
     
     /** Creates a new instance of Specification */
     public RuntimeProfile() {
@@ -60,13 +61,6 @@ public class RuntimeProfile {
         propertyChangeSupport.addPropertyChangeListener(l);
     }
     
-    /** Removes a PropertyChangeListener from the listener list.
-     * @param l The listener to remove.
-     */
-    public void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
-        propertyChangeSupport.removePropertyChangeListener(l);
-    }
-    
     /** Adds a VetoableChangeListener to the listener list.
      * @param l The listener to add.
      */
@@ -74,11 +68,40 @@ public class RuntimeProfile {
         vetoableChangeSupport.addVetoableChangeListener(l);
     }
     
-    /** Removes a VetoableChangeListener from the listener list.
-     * @param l The listener to remove.
+    /** Getter for property conformance.
+     * @return Value of property conformance.
      */
-    public void removeVetoableChangeListener(java.beans.VetoableChangeListener l) {
-        vetoableChangeSupport.removeVetoableChangeListener(l);
+    public Conformance getConformance() {
+        return this.conformance;
+    }
+    
+    /** Getter for property conformanceType.
+     * @return Value of property conformanceType.
+     */
+    public String getConformanceType() {
+        return this.conformanceType;
+    }
+    
+    /** Indexed getter for property encodings.
+     * @param index Index of the property.
+     * @return Value of the property at <CODE>index</CODE>.
+     */
+    public Encoding getEncodings(int index) {
+        return this.encodings[index];
+    }
+    
+    /** Getter for property HL7OID.
+     * @return Value of property HL7OID.
+     */
+    public String getHL7OID() {
+        return this.HL7OID;
+    }
+    
+    /** Getter for property HL7Version.
+     * @return Value of property HL7Version.
+     */
+    public String getHL7Version() {
+        return this.HL7Version;
     }
     
     /** Getter for property impNote.
@@ -88,20 +111,46 @@ public class RuntimeProfile {
         return this.impNote;
     }
     
-    /** Setter for property impNote.
-     * @param impNote New value of property impNote.
-     *
-     * @throws ProfileException
+    /** Getter for property message.
+     * @return Value of property message.
      */
-    public void setImpNote(String impNote) throws ProfileException {
-        String oldImpNote = this.impNote;
-        try {
-            vetoableChangeSupport.fireVetoableChange("impNote", oldImpNote, impNote);
-        } catch (Exception e) {
-            throw new ProfileException(null, e);
-        }
-        this.impNote = impNote;
-        propertyChangeSupport.firePropertyChange("impNote", oldImpNote, impNote);
+    public StaticDef getMessage() {
+        return this.message;
+    }
+    
+    /** Getter for property orgName.
+     * @return Value of property orgName.
+     */
+    public String getOrgName() {
+        return this.orgName;
+    }
+    
+    /** Getter for property role.
+     * @return Value of property role.
+     */
+    public String getRole() {
+        return this.role;
+    }
+    
+    /** Getter for property specName.
+     * @return Value of property specName.
+     */
+    public String getSpecName() {
+        return this.specName;
+    }
+    
+    /** Getter for property specVersion.
+     * @return Value of property specVersion.
+     */
+    public String getSpecVersion() {
+        return this.specVersion;
+    }
+    
+    /** Getter for property status.
+     * @return Value of property status.
+     */
+    public String getStatus() {
+        return this.status;
     }
     
     /** Getter for property useCase.
@@ -111,27 +160,18 @@ public class RuntimeProfile {
         return this.useCase;
     }
     
-    /** Setter for property useCase.
-     * @param useCase New value of property useCase.
-     *
-     * @throws ProfileException
+    /** Removes a PropertyChangeListener from the listener list.
+     * @param l The listener to remove.
      */
-    public void setUseCase(UseCase useCase) throws ProfileException {
-        UseCase oldUseCase = this.useCase;
-        try {
-            vetoableChangeSupport.fireVetoableChange("useCase", oldUseCase, useCase);
-        } catch (Exception e) {
-            throw new ProfileException(null, e);
-        }
-        this.useCase = useCase;
-        propertyChangeSupport.firePropertyChange("useCase", oldUseCase, useCase);
+    public void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
+        propertyChangeSupport.removePropertyChangeListener(l);
     }
     
-    /** Getter for property conformance.
-     * @return Value of property conformance.
+    /** Removes a VetoableChangeListener from the listener list.
+     * @param l The listener to remove.
      */
-    public Conformance getConformance() {
-        return this.conformance;
+    public void removeVetoableChangeListener(java.beans.VetoableChangeListener l) {
+        vetoableChangeSupport.removeVetoableChangeListener(l);
     }
     
     /** Setter for property conformance.
@@ -150,12 +190,20 @@ public class RuntimeProfile {
         propertyChangeSupport.firePropertyChange("conformance", oldConformance, conformance);
     }
     
-    /** Indexed getter for property encodings.
-     * @param index Index of the property.
-     * @return Value of the property at <CODE>index</CODE>.
+    /** Setter for property conformanceType.
+     * @param conformanceType New value of property conformanceType.
+     *
+     * @throws ProfileException
      */
-    public Encoding getEncodings(int index) {
-        return this.encodings[index];
+    public void setConformanceType(String conformanceType) throws ProfileException {
+        String oldConformanceType = this.conformanceType;
+        try {
+            vetoableChangeSupport.fireVetoableChange("conformanceType", oldConformanceType, conformanceType);
+        } catch (Exception e) {
+            throw new ProfileException(null, e);
+        }
+        this.conformanceType = conformanceType;
+        propertyChangeSupport.firePropertyChange("conformanceType", oldConformanceType, conformanceType);
     }
     
     /** Indexed setter for property encodings.
@@ -177,80 +225,20 @@ public class RuntimeProfile {
         propertyChangeSupport.firePropertyChange("encodings", null, null );
     }
     
-    /** Getter for property message.
-     * @return Value of property message.
-     */
-    public StaticDef getMessage() {
-        return this.message;
-    }
-    
-    /** Setter for property message.
-     * @param message New value of property message.
+    /** Setter for property HL7OID.
+     * @param HL7OID New value of property HL7OID.
      *
      * @throws ProfileException
      */
-    public void setMessage(StaticDef message) throws ProfileException {
-        StaticDef oldMessage = this.message;
+    public void setHL7OID(String HL7OID) throws ProfileException {
+        String oldHL7OID = this.HL7OID;
         try {
-            vetoableChangeSupport.fireVetoableChange("message", oldMessage, message);
+            vetoableChangeSupport.fireVetoableChange("HL7OID", oldHL7OID, HL7OID);
         } catch (Exception e) {
             throw new ProfileException(null, e);
         }
-        this.message = message;
-        propertyChangeSupport.firePropertyChange("message", oldMessage, message);
-    }
-    
-    /** Getter for property specName.
-     * @return Value of property specName.
-     */
-    public String getSpecName() {
-        return this.specName;
-    }
-    
-    /** Setter for property specName.
-     * @param specName New value of property specName.
-     *
-     * @throws ProfileException
-     */
-    public void setSpecName(String specName) throws ProfileException {
-        String oldSpecName = this.specName;
-        try {
-            vetoableChangeSupport.fireVetoableChange("specName", oldSpecName, specName);
-        } catch (Exception e) {
-            throw new ProfileException(null, e);
-        }
-        this.specName = specName;
-        propertyChangeSupport.firePropertyChange("specName", oldSpecName, specName);
-    }
-    
-    /** Getter for property orgName.
-     * @return Value of property orgName.
-     */
-    public String getOrgName() {
-        return this.orgName;
-    }
-    
-    /** Setter for property orgName.
-     * @param orgName New value of property orgName.
-     *
-     * @throws ProfileException
-     */
-    public void setOrgName(String orgName) throws ProfileException {
-        String oldOrgName = this.orgName;
-        try {
-            vetoableChangeSupport.fireVetoableChange("orgName", oldOrgName, orgName);
-        } catch (Exception e) {
-            throw new ProfileException(null, e);
-        }
-        this.orgName = orgName;
-        propertyChangeSupport.firePropertyChange("orgName", oldOrgName, orgName);
-    }
-    
-    /** Getter for property HL7Version.
-     * @return Value of property HL7Version.
-     */
-    public String getHL7Version() {
-        return this.HL7Version;
+        this.HL7OID = HL7OID;
+        propertyChangeSupport.firePropertyChange("HL7OID", oldHL7OID, HL7OID);
     }
     
     /** Setter for property HL7Version.
@@ -269,57 +257,70 @@ public class RuntimeProfile {
         propertyChangeSupport.firePropertyChange("HL7Version", oldHL7Version, HL7Version);
     }
     
-    /** Getter for property specVersion.
-     * @return Value of property specVersion.
-     */
-    public String getSpecVersion() {
-        return this.specVersion;
-    }
-    
-    /** Setter for property specVersion.
-     * @param specVersion New value of property specVersion.
+    /** Setter for property impNote.
+     * @param impNote New value of property impNote.
      *
      * @throws ProfileException
      */
-    public void setSpecVersion(String specVersion) throws ProfileException {
-        String oldSpecVersion = this.specVersion;
+    public void setImpNote(String impNote) throws ProfileException {
+        String oldImpNote = this.impNote;
         try {
-            vetoableChangeSupport.fireVetoableChange("specVersion", oldSpecVersion, specVersion);
+            vetoableChangeSupport.fireVetoableChange("impNote", oldImpNote, impNote);
         } catch (Exception e) {
             throw new ProfileException(null, e);
         }
-        this.specVersion = specVersion;
-        propertyChangeSupport.firePropertyChange("specVersion", oldSpecVersion, specVersion);
+        this.impNote = impNote;
+        propertyChangeSupport.firePropertyChange("impNote", oldImpNote, impNote);
     }
     
-    /** Getter for property status.
-     * @return Value of property status.
-     */
-    public String getStatus() {
-        return this.status;
-    }
-    
-    /** Setter for property status.
-     * @param status New value of property status.
+    /** Setter for property message.
+     * @param message New value of property message.
      *
      * @throws ProfileException
      */
-    public void setStatus(String status) throws ProfileException {
-        String oldStatus = this.status;
+    public void setMessage(StaticDef message) throws ProfileException {
+        StaticDef oldMessage = this.message;
         try {
-            vetoableChangeSupport.fireVetoableChange("status", oldStatus, status);
+            vetoableChangeSupport.fireVetoableChange("message", oldMessage, message);
         } catch (Exception e) {
             throw new ProfileException(null, e);
         }
-        this.status = status;
-        propertyChangeSupport.firePropertyChange("status", oldStatus, status);
+        this.message = message;
+        propertyChangeSupport.firePropertyChange("message", oldMessage, message);
     }
     
-    /** Getter for property role.
-     * @return Value of property role.
+    public void setName(String theName) throws ProfileException {
+        String oldOrgName = this.name;
+        try {
+            vetoableChangeSupport.fireVetoableChange("name", oldOrgName, theName);
+        } catch (Exception e) {
+            throw new ProfileException(null, e);
+        }
+        this.name = theName;
+        propertyChangeSupport.firePropertyChange("name", oldOrgName, theName);
+	}
+    
+    /**
+	 * @return the name for this profile (as drawn from the MetaData tag within the overall profile tag)
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/** Setter for property orgName.
+     * @param orgName New value of property orgName.
+     *
+     * @throws ProfileException
      */
-    public String getRole() {
-        return this.role;
+    public void setOrgName(String orgName) throws ProfileException {
+        String oldOrgName = this.orgName;
+        try {
+            vetoableChangeSupport.fireVetoableChange("orgName", oldOrgName, orgName);
+        } catch (Exception e) {
+            throw new ProfileException(null, e);
+        }
+        this.orgName = orgName;
+        propertyChangeSupport.firePropertyChange("orgName", oldOrgName, orgName);
     }
     
     /** Setter for property role.
@@ -338,50 +339,68 @@ public class RuntimeProfile {
         propertyChangeSupport.firePropertyChange("role", oldRole, role);
     }
     
-    /** Getter for property HL7OID.
-     * @return Value of property HL7OID.
-     */
-    public String getHL7OID() {
-        return this.HL7OID;
-    }
-    
-    /** Setter for property HL7OID.
-     * @param HL7OID New value of property HL7OID.
+    /** Setter for property specName.
+     * @param specName New value of property specName.
      *
      * @throws ProfileException
      */
-    public void setHL7OID(String HL7OID) throws ProfileException {
-        String oldHL7OID = this.HL7OID;
+    public void setSpecName(String specName) throws ProfileException {
+        String oldSpecName = this.specName;
         try {
-            vetoableChangeSupport.fireVetoableChange("HL7OID", oldHL7OID, HL7OID);
+            vetoableChangeSupport.fireVetoableChange("specName", oldSpecName, specName);
         } catch (Exception e) {
             throw new ProfileException(null, e);
         }
-        this.HL7OID = HL7OID;
-        propertyChangeSupport.firePropertyChange("HL7OID", oldHL7OID, HL7OID);
+        this.specName = specName;
+        propertyChangeSupport.firePropertyChange("specName", oldSpecName, specName);
     }
     
-    /** Getter for property conformanceType.
-     * @return Value of property conformanceType.
-     */
-    public String getConformanceType() {
-        return this.conformanceType;
-    }
-    
-    /** Setter for property conformanceType.
-     * @param conformanceType New value of property conformanceType.
+    /** Setter for property specVersion.
+     * @param specVersion New value of property specVersion.
      *
      * @throws ProfileException
      */
-    public void setConformanceType(String conformanceType) throws ProfileException {
-        String oldConformanceType = this.conformanceType;
+    public void setSpecVersion(String specVersion) throws ProfileException {
+        String oldSpecVersion = this.specVersion;
         try {
-            vetoableChangeSupport.fireVetoableChange("conformanceType", oldConformanceType, conformanceType);
+            vetoableChangeSupport.fireVetoableChange("specVersion", oldSpecVersion, specVersion);
         } catch (Exception e) {
             throw new ProfileException(null, e);
         }
-        this.conformanceType = conformanceType;
-        propertyChangeSupport.firePropertyChange("conformanceType", oldConformanceType, conformanceType);
+        this.specVersion = specVersion;
+        propertyChangeSupport.firePropertyChange("specVersion", oldSpecVersion, specVersion);
+    }
+    
+    /** Setter for property status.
+     * @param status New value of property status.
+     *
+     * @throws ProfileException
+     */
+    public void setStatus(String status) throws ProfileException {
+        String oldStatus = this.status;
+        try {
+            vetoableChangeSupport.fireVetoableChange("status", oldStatus, status);
+        } catch (Exception e) {
+            throw new ProfileException(null, e);
+        }
+        this.status = status;
+        propertyChangeSupport.firePropertyChange("status", oldStatus, status);
+    }
+
+	/** Setter for property useCase.
+     * @param useCase New value of property useCase.
+     *
+     * @throws ProfileException
+     */
+    public void setUseCase(UseCase useCase) throws ProfileException {
+        UseCase oldUseCase = this.useCase;
+        try {
+            vetoableChangeSupport.fireVetoableChange("useCase", oldUseCase, useCase);
+        } catch (Exception e) {
+            throw new ProfileException(null, e);
+        }
+        this.useCase = useCase;
+        propertyChangeSupport.firePropertyChange("useCase", oldUseCase, useCase);
     }
     
 }
