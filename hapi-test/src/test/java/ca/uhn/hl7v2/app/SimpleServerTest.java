03ef2113b214bf976443b60a91d54d7b4b41246c
ohr <ohr@users.noreply.github.com>
2012-01-30 11:58:42 +0000

    Refactorings to address concurrency

--- hapi-test/src/test/java/ca/uhn/hl7v2/app/SimpleServerTest.java
+++ hapi-test/src/test/java/ca/uhn/hl7v2/app/SimpleServerTest.java
@@ -1,93 +1,95 @@
 package ca.uhn.hl7v2.app;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import junit.framework.Assert;
-import ca.uhn.hl7v2.HL7Exception;
-import ca.uhn.hl7v2.llp.MinLowerLayerProtocol;
-import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.util.RandomServerPortProvider;
 
 public class SimpleServerTest implements ConnectionListener {
 
-	private int myConnects = 0;
-	private int myDisconnects = 0;
+	private CountDownLatch connectLatch;
+	private CountDownLatch disconnectLatch;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SimpleServerTest.class);
+	
+	@Before
+	public void setup() {
+		connectLatch = new CountDownLatch(1);
+		disconnectLatch = new CountDownLatch(1);
+	}
 
 	@Test
-	public void testDetectConnectAndDisconnect() throws Throwable {
+	public void testDetectConnectAndDisconnect() throws IOException, InterruptedException {
 
 		int port = RandomServerPortProvider.findFreePort();
-		SimpleServer ss = new SimpleServer(port);
+		SimpleTestServer ss = new SimpleTestServer(port);
 		ss.registerConnectionListener(this);
 		ss.start();
-		Thread.sleep(1000);
-
-		Assert.assertEquals(0, myConnects);
-		Assert.assertEquals(0, myDisconnects);
-
-		Socket con = new Socket();
-
-		/*
-		 * SimpleServer does most of its work in separate threads, so
-		 * we retry things just in case there is some lag 
-		 */
-		int retries = 1000000;
+		assertTrue("Server did not start up", ss.waitUntilRunning());
+		LOG.info("Received notification by server");
 		
-		for (int i = 0;; i++) {
-			try {
-				con.connect(new InetSocketAddress("localhost", port), 2000);
-				break;
-			} catch (Throwable e) {
-				if (i == retries) {
-					throw e;
-				} else {
-					Thread.sleep(100);
-				}
-			}
-		}
+		assertEquals(1, connectLatch.getCount());
+		assertEquals(1, disconnectLatch.getCount());
 
-		for (int i = 0;; i++) {
-			try {
-				Assert.assertEquals(1, myConnects);
-				Assert.assertEquals(0, myDisconnects);
-				break;
-			} catch (Throwable e) {
-				if (i == retries) {
-					throw e;
-				} else {
-					Thread.sleep(100);
-				}
-			}
-		}
-
+		LOG.info("Creating connection");
+		Socket con = new Socket();
+		con.connect(new InetSocketAddress("localhost", port), 2000);
+		assertTrue(connectLatch.await(1000, TimeUnit.MILLISECONDS));
+		assertEquals(1, disconnectLatch.getCount());
+		
+		LOG.info("Closing connection");
 		con.close();
-
-		for (int i = 0;; i++) {
-			try {
-				Assert.assertEquals(1, myConnects);
-				Assert.assertEquals(1, myDisconnects);
-				break;
-			} catch (Throwable e) {
-				if (i == retries) {
-					throw e;
-				} else {
-					Thread.sleep(100);
-				}
-			}
-		}
-
+		assertTrue(disconnectLatch.await(1000, TimeUnit.MILLISECONDS));
+		assertEquals(0, connectLatch.getCount());
+		
+		LOG.info("done");
 	}
 
 	public void connectionReceived(Connection theC) {
-		myConnects++;
+		LOG.info("Connection received by client");
+		connectLatch.countDown();
 	}
 
 	public void connectionDiscarded(Connection theC) {
-		myDisconnects++;
+		LOG.info("Connection disposed by client");
+		disconnectLatch.countDown();
+	}
+	
+	
+	/**
+	 * Little test server that provides a hook to determine if
+	 * everything is up and running.
+	 */
+	private class SimpleTestServer extends SimpleServer {
+
+		private CountDownLatch upAndRunning = new CountDownLatch(1);
+		
+		public SimpleTestServer(int port) {
+			super(port);
+		}
+
+		@Override
+		protected void afterStartup() {
+			super.afterStartup();
+			LOG.info("Server is up and running");
+			upAndRunning.countDown();
+		}
+		
+		public boolean waitUntilRunning() throws InterruptedException {
+			return upAndRunning.await(2000, TimeUnit.MILLISECONDS);
+		}
+		
 	}
 
 }
