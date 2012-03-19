e46fcd1fc13378745fa1e7cc7fbff9feab48b1f6
ohr <ohr@users.noreply.github.com>
2012-03-19 13:05:32 +0000

    3323850: added test but everything seems to be o.k.

--- hapi-test/src/test/java/ca/uhn/hl7v2/parser/EscapeTest.java
+++ hapi-test/src/test/java/ca/uhn/hl7v2/parser/EscapeTest.java
@@ -1,29 +1,29 @@
 package ca.uhn.hl7v2.parser;
 
- import java.io.BufferedReader;
+ import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.v23.message.ORU_R01;
 import ca.uhn.hl7v2.model.v24.datatype.FT;
+import ca.uhn.hl7v2.validation.impl.NoValidation;
 import ca.uhn.hl7v2.validation.impl.ValidationContextImpl;
 
 /**
  * JUnit test cases for Escape.  
  * @author bryan
  */
-public class EscapeTest extends TestCase {
+public class EscapeTest {
     
-    private EncodingCharacters enc;
+    private EncodingCharacters enc = new EncodingCharacters('|', null);
     
-    public EscapeTest(java.lang.String testName) {
-        super(testName);
-        this.enc = new EncodingCharacters('|', null);
-    }
-    
+	@Test
     public void testUnescape() throws Exception {
         String uu = getUuencodedEscapedString();
         Escape.unescape(uu, enc); 
@@ -34,6 +34,7 @@ public class EscapeTest extends TestCase {
         //out.close();
     }
     
+	@Test
     public void testSimpleEscape() {
         String actual = Escape.escape("GLUCOSE^1H POST 75 G GLUCOSE PO:SCNC:PT:SER/PLAS:QN", enc);
         String expected = "GLUCOSE\\S\\1H POST 75 G GLUCOSE PO:SCNC:PT:SER/PLAS:QN";
@@ -69,6 +70,7 @@ public class EscapeTest extends TestCase {
 //        return IOUtil.toString(in);
 //    }
 
+	@Test
 	public void testFormattingCharacters() throws HL7Exception {
 
 		String msg = "MSH|^~\\&|ULTRA|TML|OLIS|OLIS|200911231509||ORU|5951|T|2.3\r" +
@@ -91,7 +93,34 @@ public class EscapeTest extends TestCase {
 		assertEquals(msg.trim(), encoded.trim());
 
 	}
+	
+	// Issue 3323850
+	@Test
+	public void testFormattingCharacters2() throws HL7Exception {
 
+		String msg = "MSH|^~\\&|PHCN_ULTRA|2220|HSIE|2220|201106161233||ORU^R01|72313573|T|2.4|||AL|AL|AU\r" +
+					 "PV1||I|^DIS^DIS^2220|||||0129296H^BRAUN^GARY|7MPH^MPH-HL7-RESULT FEED|||||||||I|^^^2220\r" +
+					 "ORC|RE|^HNAM_ORDERID|11-6879530-GAS-0^PHCN_ULTRA||RE\r" +
+					 "OBR|1|^HNAM_ORDERID|11-6879530-GAS-0^PHCN_ULTRA|GAS^GASES (BLOOD)|||201106161000|||||||||0129296H^BRAUN^GARY^^^DR^^^2220^^^^Provider Num||||1295102|7MPH|201106161233||GRP|F||^^^201106161203\r" +
+					 "OBX|1|FT|GAS^^LN||Biochemistry (Whole Blood Sample) - Type Venous\\.br\\\\.br\\ Analysis Date : 16/06/2011\\.br\\ Analysis Time : 12:21\\.br\\\\.br\\ Measured Parameters :\\.br\\ pH : \\H\\ 7.28\\N\\ (7.38-7.43) *\\.br\\ pCO2 : \\H\\ 117\\N\\ mmHg (35-45) ***\\.br\\ pO2 : \\H\\ 37\\N\\ mmHg\\.br\\ Sodium : 140 mmol/L (136-142)\\.br\\ Potassium : 3.8 mmol/L (3.8-5.0)\\.br\\ Chloride : \\H\\ 83\\N\\ mmol/L (95-110) *\\.br\\ Ionised Calcium : 1.23 mmol/L (1.15-1.29)\\.br\\ Lactate : \\H\\ 2.1\\N\\ mmol/L (0.5-2.0) *\\.br\\ Glucose : \\H\\ 10.9\\N\\ mmol/L (4.0-7.8) *\\.br\\ Haemoglobin : 13.9 g/dL (11.5-16.5)\\.br\\ FO2Hb : \\H\\ 60.7\\N\\ % (94.0-97.0) *\\.br\\ FCOHb : \\H\\ 2.1\\N\\ % (< 1.5) *\\.br\\ FmetHb : 0.5 % (< 1.5)\\.br\\ FHHb : \\H\\ 36.7\\N\\ % (< 5.0) *\\.br\\ Haematocrit : 42.6 % (37.0-47.0)\\.br\\\\.br\\ Calculated (37 \\S\\o C) :\\.br\\ Actual HCO3 : \\H\\ 54\\N\\ mmol/L (20-24) ***\\.br\\ Standard HCO3 : \\H\\ 43\\N\\ mmol/L (22-26) *\\.br\\ Base Excess : \\H\\ +19.6\\N\\ mmol/L (-3.3 - +1.2) *\\.br\\ Oxygen Saturation : \\H\\ 62.5\\N\\ %\\.br\\\\.br\\ Note: The reference intervals for pH, pCO2 and pO2 apply for arterial\\.br\\ samples only.|||A|||F ";
+
+		// ca.uhn.hl7v2.model.v24.message.ORU_R01 message = new ca.uhn.hl7v2.model.v24.message.ORU_R01();
+		Parser p = new PipeParser();
+		p.setValidationContext(new NoValidation());
+
+		ca.uhn.hl7v2.model.v24.message.ORU_R01 message = (ca.uhn.hl7v2.model.v24.message.ORU_R01)p.parse(msg);
+		
+		int intCountOBR = message.getPATIENT_RESULT().getORDER_OBSERVATIONReps();
+		for (int i = 0; i < intCountOBR; i++) {
+			message.getPATIENT_RESULT().getORDER_OBSERVATION(i).getOBR().removeObr16_OrderingProvider(0);
+		}
+
+		String encoded = message.encode();
+		System.out.println("Encoded message: \r\n" + encoded);
+		assertEquals(msg.trim(), encoded.trim());
+	}	
+
+	@Test
 	public void testPreserveFormattingChars() throws HL7Exception, IOException {
 		
 		ORU_R01 msg = new ORU_R01();
