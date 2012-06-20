f53b40a5404123e5b4fada4c5c398b32f8246bdb
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-06-20 01:52:48 +0000

    Improve error messages for parsing null

--- trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/model/AbstractSegmentTest.java
+++ trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/model/AbstractSegmentTest.java
@@ -1,9 +1,11 @@
 package ca.uhn.hl7v2.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.v22.message.ADT_A01;
 import ca.uhn.hl7v2.model.v23.message.ORU_R01;
-import junit.framework.*;
 import ca.uhn.hl7v2.model.v24.segment.MSH;
 import ca.uhn.hl7v2.parser.DefaultModelClassFactory;
 import ca.uhn.hl7v2.parser.ModelClassFactory;
@@ -12,25 +14,13 @@ import ca.uhn.hl7v2.parser.ModelClassFactory;
  * JUnit test cases for AbstractSegment
  * @author bryan
  */
-public class AbstractSegmentTest extends TestCase {
+public class AbstractSegmentTest {
     private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(AbstractSegmentTest.class);
-    
-    public AbstractSegmentTest(java.lang.String testName) {
-        super(testName);
-    }
-    
-    public static void main(java.lang.String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-    
-    public static Test suite() {
-        TestSuite suite = new TestSuite(AbstractSegmentTest.class);
-        return suite;
-    }
     
     // Add test methods here, they have to start with 'test' name.
     // for example:
     // public void testHello() {}
+	@Test
     public void testEnsureEnoughFields() throws Exception {
         ModelClassFactory factory = new DefaultModelClassFactory();
         MSH msh = new MSH(new GenericMessage.V25(factory), factory);
@@ -41,6 +31,7 @@ public class AbstractSegmentTest extends TestCase {
         assertEquals("ca.uhn.hl7v2.model.Varies", msh.getField(n+3, 0).getClass().getName());
     }
     
+	@Test
     public void testUnexpectedFieldReps() throws Exception {
         ModelClassFactory factory = new DefaultModelClassFactory();
         MSH msh = new MSH(new GenericMessage.V25(factory), new DefaultModelClassFactory());
@@ -50,6 +41,7 @@ public class AbstractSegmentTest extends TestCase {
     }
 
 
+	@Test
 	public void testParseEmpty() throws HL7Exception {
 
         String string = "MSH|^~\\&|LABGL1||DMCRES||19951002185200||ADT^A01|LABGL1199510021852632|P|2.2\r"
@@ -74,7 +66,25 @@ public class AbstractSegmentTest extends TestCase {
 
 	}
 
+	@Test
+	public void testParseNull() throws HL7Exception {
 
+        String string = "MSH|^~\\&|LABGL1||DMCRES||19951002185200||ADT^A01|LABGL1199510021852632|P|2.2\r"
+                + "PID|||T12345||TEST^PATIENT^P||19601002|M||||||||||123456\r"
+                + "PV1|||NER|||||||GSU||||||||E||||||||||||||||||||||||||19951002174900|19951006\r";
+        ADT_A01 a01 = new ADT_A01();
+        a01.parse(string);
+
+        try {
+        	a01.getPID().parse(null);
+        	fail();
+        } catch (NullPointerException e) {
+        	// expected
+        }
+
+	}
+
+	@Test
 	public void testInsertRepetition() throws HL7Exception {
 
         String string = "MSH|^~\\&|ULTRA|TML|OLIS|OLIS|200905011130||ORU^R01|20169838|T|2.3\r"
@@ -87,17 +97,18 @@ public class AbstractSegmentTest extends TestCase {
         ORU_R01 msg = new ORU_R01();
         msg.parse(string);
         
-        Assert.assertEquals("PID|||1000", msg.getRESPONSE(0).getPATIENT().getPID().encode());
+        assertEquals("PID|||1000", msg.getRESPONSE(0).getPATIENT().getPID().encode());
         
         msg.getRESPONSE(0).getPATIENT().getPID().insertPid3_PatientIDInternalID(1).parse("1001");
-        Assert.assertEquals("PID|||1000~1001", msg.getRESPONSE(0).getPATIENT().getPID().encode());
+        assertEquals("PID|||1000~1001", msg.getRESPONSE(0).getPATIENT().getPID().encode());
 
         msg.getRESPONSE(0).getPATIENT().getPID().insertPid3_PatientIDInternalID(0).parse("0999");
-        Assert.assertEquals("PID|||0999~1000~1001", msg.getRESPONSE(0).getPATIENT().getPID().encode());
+        assertEquals("PID|||0999~1000~1001", msg.getRESPONSE(0).getPATIENT().getPID().encode());
 
 	}
 	
 	
+	@Test
 	public void testParseMshWithNoContent() {
 		
 		try {
