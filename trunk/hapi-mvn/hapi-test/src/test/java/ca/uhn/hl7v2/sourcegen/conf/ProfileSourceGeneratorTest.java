3e6754fe4e5961a089a5d0500994fe1071747e81
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-07-13 12:03:24 +0000

    First test for namespaces in generated conformance structures. This feature isn't complete yet, but this tests the first part

--- trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/sourcegen/conf/ProfileSourceGeneratorTest.java
+++ trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/sourcegen/conf/ProfileSourceGeneratorTest.java
@@ -1,10 +1,16 @@
 package ca.uhn.hl7v2.sourcegen.conf;
 
+import org.junit.Test;
+
+import ca.uhn.hl7v2.model.v25.datatype.IS;
 import ca.uhn.hl7v2.sourcegen.SegmentElement;
 import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
-public class ProfileSourceGeneratorTest extends TestCase {
 
+public class ProfileSourceGeneratorTest {
+
+	@Test
 	public void testExtractTableNamespace() {
 		
     	SegmentElement e = new SegmentElement("", "", 0);
@@ -19,6 +25,15 @@ public class ProfileSourceGeneratorTest extends TestCase {
     	assertEquals(null, e.tableNamespace);
     	assertEquals(123, e.table);
 		
+	}
+	
+	@Test
+	public void testGenerateSourceWithNamespace() {
+		ca.uhn.hl7v2.test.withns.message.ADT_A01 msg = new ca.uhn.hl7v2.test.withns.message.ADT_A01();
+		assertEquals(399, msg.getMSH().getCountryCode().getTable());
+		assertEquals("TESTNS", msg.getMSH().getCountryCode().getTableNamespace());
+		
+		IS nsid = msg.getMSH().getSendingApplication().getHd1_NamespaceID();
 	}
 	
 }
