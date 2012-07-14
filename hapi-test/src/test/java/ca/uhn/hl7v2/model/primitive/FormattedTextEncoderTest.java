260c288be92e063987cc898a7cfdffc5540eba8a
James Agnew <jamesagnew@gmail.com>
2012-07-14 18:33:43 +0000

    Finish refactoring HTML encoding into FormattedTextEncoder

--- hapi-test/src/test/java/ca/uhn/hl7v2/model/primitive/FormattedTextEncoderTest.java
+++ hapi-test/src/test/java/ca/uhn/hl7v2/model/primitive/FormattedTextEncoderTest.java
@@ -7,29 +7,24 @@ import ca.uhn.hl7v2.model.v25.datatype.FT;
 import ca.uhn.hl7v2.model.v25.message.ADT_A01;
 import static org.junit.Assert.*;
 
-public class AbstractTextPrimitiveTest {
+public class FormattedTextEncoderTest {
 
-	/**
-	 * From ATP:
-	 * <li>\.br\
-	 * <li>\.ce\
-	 * <li>\.sk\
-	 * <li>\.sp\
-	 * <li>\.sp ###\
-	 * <li>\.fi\
-	 * <li>\.nf\
-	 * <li>\.in\
-	 * <li>\.ti\
-	 * <li>\H\
-	 * <li>\N\
-	 * <li>Ampersands (&amp;) are converted to their HTML equivalent (&amp;amp;)
-	 * <li>Chars over ASCII 160 are HTML encoded (e.g. &amp;#199;) 
-	 */
+	@Test
+	public void testGetValueAsHtmlHighAscii() throws DataTypeException {
+		ADT_A01 msg = new ADT_A01();
+		FT ft = new FT(msg);
+
+		String value = "hello " + ((char)177);
+		ft.setValue(value);
+		assertEquals("hello &#177;", ft.getValueAsHtml());
+
+	}
+
 	@Test
 	public void testGetValueAsHtmlBr() throws DataTypeException {
 		ADT_A01 msg = new ADT_A01();
 		FT ft = new FT(msg);
-		
+
 		String value = "some text\\.br\\line 2\\.br\\line 3";
 		ft.setValue(value);
 		assertEquals("some text<br>line 2<br>line 3", ft.getValueAsHtml());
@@ -39,12 +34,12 @@ public class AbstractTextPrimitiveTest {
 		assertEquals("some text<br>line 2<br>line 3", ft.getValueAsHtml());
 
 	}
-	
+
 	@Test
 	public void testGetValueAsHtmlCe() throws DataTypeException {
 		ADT_A01 msg = new ADT_A01();
 		FT ft = new FT(msg);
-		
+
 		String value = "this line isn't centered\\.ce\\this line is centered\\.br\\also not";
 		ft.setValue(value);
 		assertEquals("this line isn't centered<br><center>this line is centered</center><br>also not", ft.getValueAsHtml());
@@ -64,15 +59,40 @@ public class AbstractTextPrimitiveTest {
 		value = "\\.ce\\this line is centered\\.ce\\so is this";
 		ft.setValue(value);
 		assertEquals("<center>this line is centered</center><br><center>so is this</center>", ft.getValueAsHtml());
-		
+
 	}
 
+	@Test
+	public void testGetValueAsHtmlAmpersands() throws DataTypeException {
+		ADT_A01 msg = new ADT_A01();
+		FT ft = new FT(msg);
+
+		String value = "peas&carrots";
+		ft.setValue(value);
+		assertEquals("peas&amp;carrots", ft.getValueAsHtml());
+	}
+
+	@Test
+	public void testGetValueAsHtmlHN() throws DataTypeException {
+		ADT_A01 msg = new ADT_A01();
+		FT ft = new FT(msg);
+
+		String value = "start\\H\\bold\\N\\not";
+		ft.setValue(value);
+		assertEquals("start<b>bold</b>not", ft.getValueAsHtml());
+
+		// Make sure no dangling <b> tags are left over
+		value = "start\\H\\\\H\\bold\\N\\not";
+		ft.setValue(value);
+		assertEquals("start<b><b>bold</b></b>not", ft.getValueAsHtml());
+
+	}
 
 	@Test
 	public void testGetValueAsHtmlNfFi() throws DataTypeException {
 		ADT_A01 msg = new ADT_A01();
 		FT ft = new FT(msg);
-		
+
 		String value = "word wrap\\.nf\\don't word wrap\\.fi\\word wrap";
 		ft.setValue(value);
 		assertEquals("word wrap<nobr>don't word wrap</nobr>word wrap", ft.getValueAsHtml());
@@ -86,12 +106,67 @@ public class AbstractTextPrimitiveTest {
 		assertEquals("word wrap<nobr>don't word wrap<br>also don't</nobr>word wrap", ft.getValueAsHtml());
 
 	}
-	
+
+	@Test
+	public void testGetValueAsHtmlSk() throws DataTypeException {
+		ADT_A01 msg = new ADT_A01();
+		FT ft = new FT(msg);
+
+		String value = "text\\.sk 5\\more";
+		ft.setValue(value);
+		assertEquals("text&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;more", ft.getValueAsHtml());
+
+		value = "text\\.sk 0\\more";
+		ft.setValue(value);
+		assertEquals("textmore", ft.getValueAsHtml());
+
+	}
+
+	@Test
+	public void testGetValueAsHtmlInTi() throws DataTypeException {
+		ADT_A01 msg = new ADT_A01();
+		FT ft = new FT(msg);
+
+		// String value2 = "text\\.br\\\\.in+5\\more\\.br\\still";
+		// ft.setValue(value2);
+		// String valueAsHtml = ft.getValueAsHtml();
+		// System.out.println(valueAsHtml);
+		// assertEquals("text<div style=\"margin-left: 5em;\">more</div><div style=\\\"margin-left: 5em;\\\">still</div>",
+		// valueAsHtml);
+		//
+
+		// Need a \\.br\\ before the \\.in 5\\
+		String value = "text\\.in 5\\more";
+		ft.setValue(value);
+		assertEquals(value, ft.getValueAsHtml());
+
+		value = "text\\.br\\\\.in 5\\more";
+		ft.setValue(value);
+		assertEquals("text<div style=\"margin-left: 5em;\">more</div>", ft.getValueAsHtml());
+
+		value = "text\\.br\\\\.in+5\\more";
+		ft.setValue(value);
+		assertEquals("text<div style=\"margin-left: 5em;\">more</div>", ft.getValueAsHtml());
+
+		value = "text\\.br\\\\.in-5\\more";
+		ft.setValue(value);
+		assertEquals("text<br>more", ft.getValueAsHtml());
+
+		value = "text\\.br\\\\.in+5\\more\\.br\\still";
+		ft.setValue(value);
+		assertEquals("text<div style=\"margin-left: 5em;\">more</div><div style=\"margin-left: 5em;\">still</div>", ft.getValueAsHtml());
+
+		value = "text\\.br\\\\.in+5\\more\\.br\\\\.ti-2\\still";
+		ft.setValue(value);
+		assertEquals("text<div style=\"margin-left: 5em;\">more</div><div style=\"margin-left: 3em;\">still</div>", ft.getValueAsHtml());
+
+	}
+
 	@Test
 	public void testGetValueAsHtmlSp() throws DataTypeException {
 		ADT_A01 msg = new ADT_A01();
 		FT ft = new FT(msg);
-		
+
 		String value = "text\\.sp\\more text";
 		ft.setValue(value);
 		assertEquals("text<br>&nbsp;&nbsp;&nbsp;&nbsp;more text", ft.getValueAsHtml());
@@ -109,7 +184,7 @@ public class AbstractTextPrimitiveTest {
 		assertEquals("text<br><br>&nbsp;&nbsp;&nbsp;&nbsp;more<br>not<br>&nbsp;&nbsp;&nbsp;three", ft.getValueAsHtml());
 
 		// junk at the end should be preserved.. i guess?
-		
+
 		value = "text\\.sp";
 		ft.setValue(value);
 		assertEquals(value, ft.getValueAsHtml());
@@ -125,8 +200,7 @@ public class AbstractTextPrimitiveTest {
 		value = "text\\.sp \\";
 		ft.setValue(value);
 		assertEquals(value, ft.getValueAsHtml());
-		
-	}	
-	
-	
+
+	}
+
 }
