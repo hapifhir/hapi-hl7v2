327be78c5b1d68863b5e8d58c90acd4214825421
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-07-16 02:44:34 +0000

    Fix deployment problem

--- trunk/hapi-mvn/scripts/deploy_site.sh
+++ trunk/hapi-mvn/scripts/deploy_site.sh
@@ -4,7 +4,7 @@ cd target/site
 rm ../site.tgz
 tar zcvf ../site.tgz *
 cd ..
-#ssh -t jamesagnew,hl7api@shell.sourceforge.net create
+ssh -t jamesagnew,hl7api@shell.sourceforge.net create
 scp site.tgz jamesagnew,hl7api@shell.sourceforge.net:/home/project-web/hl7api/htdocs/
 echo site is at /home/project-web/hl7api/htdocs/
 ssh -t jamesagnew,hl7api@shell.sourceforge.net
