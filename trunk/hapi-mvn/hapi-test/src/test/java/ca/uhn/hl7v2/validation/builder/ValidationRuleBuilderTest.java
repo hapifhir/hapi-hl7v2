7fa6cd73de1ad3137f288f2dc43026a0d98beda1
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-08-08 16:38:25 +0000

    Fix import

--- trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/validation/builder/ValidationRuleBuilderTest.java
+++ trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/validation/builder/ValidationRuleBuilderTest.java
@@ -9,13 +9,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import ca.uhn.hl7v2.Version;
-import edu.emory.mathcs.backport.java.util.Arrays;
 
 public class ValidationRuleBuilderTest {
 
