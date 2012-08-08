8cf5164b1a596f7db24fcae7a13e19ce4f78f96b
ohr <ohr@users.noreply.github.com>
2012-08-08 12:35:15 +0000

    Use 'official' way to check for proper DOM implementation

--- hapi-base/src/main/java/ca/uhn/hl7v2/VersionLogger.java
+++ hapi-base/src/main/java/ca/uhn/hl7v2/VersionLogger.java
@@ -5,47 +5,51 @@ import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMImplementation;
+
+import ca.uhn.hl7v2.util.XMLUtils;
 
 /**
  * Class to log the HAPI version when HAPI is first used (mostly for troubleshooting purposes)
  */
 public class VersionLogger {
 
-    private static boolean ourInitialized = false;
+	private static boolean ourInitialized = false;
 	private static String ourVersion;
-    private static final Logger LOG = LoggerFactory.getLogger(VersionLogger.class);
-    
-    /**
-     * Non-instantiable
-     */
-    private VersionLogger() {
-        // nothing
-    }
-    
-    /**
-     * Logs the HAPI version on the first time this method is invoked, does nothing afterwards
-     */
-    public static void init() {
-        if (!ourInitialized) {
-            try {
-                InputStream is = VersionLogger.class.getResourceAsStream("/ca/uhn/hl7v2/hapi-version.properties");
-                Properties p = new Properties();
-                p.load(is);
-                ourVersion = p.getProperty("version");
+	private static final Logger LOG = LoggerFactory.getLogger(VersionLogger.class);
+
+	/**
+	 * Non-instantiable
+	 */
+	private VersionLogger() {
+		// nothing
+	}
+
+	/**
+	 * Logs the HAPI version on the first time this method is invoked, does nothing afterwards
+	 */
+	public static void init() {
+		if (!ourInitialized) {
+			try {
+				InputStream is = VersionLogger.class
+						.getResourceAsStream("/ca/uhn/hl7v2/hapi-version.properties");
+				Properties p = new Properties();
+				p.load(is);
+				ourVersion = p.getProperty("version");
 				LOG.info("HAPI version is: " + ourVersion);
-				
-				Class<?> clazz = Class.forName("org.w3c.dom.ls.DOMImplementationLS");
-				try {
-					clazz.getMethod("createLSParser", short.class, String.class);
-				} catch (Exception e) {
-					LOG.warn("You appear to have a very old version of Xerces on your classpath! Please be warned that XML parsing and encoding may fail.");
+
+				// Check if proper XML support
+				DOMImplementation impl = XMLUtils.getDOMImpl();
+				if (impl == null) {
+					LOG.warn("DOM Level 3 (Load and Save) is NOT supported by the XML library found first on your classpath!");
+					LOG.warn("XML parsing and encoding as well as working with Conformance Profiles will fail.");
 				}
-            } catch (Exception e) {
-                // ignore
-            }
-            ourInitialized = true;
-        }
-    }
+			} catch (Exception e) {
+				// ignore
+			}
+			ourInitialized = true;
+		}
+	}
 
 	/**
 	 * @return Returns the current version of HAPI
@@ -53,5 +57,5 @@ public class VersionLogger {
 	public static String getVersion() {
 		return ourVersion;
 	}
-    
+
 }
