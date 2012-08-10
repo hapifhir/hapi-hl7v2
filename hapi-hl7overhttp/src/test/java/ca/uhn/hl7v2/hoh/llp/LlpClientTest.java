db62ec68aaff6db32ebeb1f1ca16c3a12b51c489
James Agnew <jamesagnew@gmail.com>
2012-08-10 18:37:06 +0000

    Fix unit test

--- hapi-hl7overhttp/src/test/java/ca/uhn/hl7v2/hoh/llp/LlpClientTest.java
+++ hapi-hl7overhttp/src/test/java/ca/uhn/hl7v2/hoh/llp/LlpClientTest.java
@@ -27,7 +27,7 @@ public class LlpClientTest {
 	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(LlpClientTest.class);
 	private static int ourPort;
 	private static SingleCredentialServerCallback ourServerCallback;
-	private static TestServerSocketThread ourServerSocketThread;
+	private static ServerSocketThreadForTesting ourServerSocketThread;
 	private static ExecutorService ourExecutor;
 
 	@Test
@@ -75,7 +75,7 @@ public class LlpClientTest {
 		ourLlp.setAuthorizationCallback(new SingleCredentialClientCallback("hello", "hapiworld"));
 		ourServerCallback = new SingleCredentialServerCallback("hello", "hapiworld");
 
-		ourServerSocketThread = new TestServerSocketThread(ourPort, ourServerCallback);
+		ourServerSocketThread = new ServerSocketThreadForTesting(ourPort, ourServerCallback);
 		ourServerSocketThread.start();
 		ourServerSocketThread.getLatch().await();
 		
