9e6e9170b1d44240e3de17dfa527bb465143b834
James Agnew <jamesagnew@gmail.com>
2012-08-10 21:55:44 +0000

    More documentation

--- hapi-hl7overhttp/src/test/java/ca/uhn/hl7v2/hoh/sockets/CustomCertificateTlsSocketFactoryTest.java
+++ hapi-hl7overhttp/src/test/java/ca/uhn/hl7v2/hoh/sockets/CustomCertificateTlsSocketFactoryTest.java
@@ -9,99 +9,111 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
 
+import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLServerSocket;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.security.SslSelectChannelConnector;
 
 import ca.uhn.hl7v2.hoh.util.RandomServerPortProvider;
 
 public class CustomCertificateTlsSocketFactoryTest {
 
+	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(CustomCertificateTlsSocketFactoryTest.class);
+
 	private int myPort;
+
 
 	@Before
 	public void before() {
 		myPort = RandomServerPortProvider.findFreePort();
-
 	}
 
-	@Test
-	public void testConnectToTeustedocket() throws IOException, InterruptedException {
-		
-//		CustomCertificateTlsSocketFactory badServer = new CustomCertificateTlsSocketFactory();
-//		badServer.setKeystoreFilename("src/test/resources/symmetrickey.jks");
-//		badServer.setKeystorePassphrase("thestorepass");
-//		badServer.setKeystoreType("JCEKS");
-//		Receiver receiver = new Receiver(badServer);
-//		receiver.start();
-//		Thread.sleep(500);
-//		
-//		CustomCertificateTlsSocketFactory goodClient = new CustomCertificateTlsSocketFactory();
-//		goodClient.setKeystoreFilename("src/test/resources/symmetrickey.jks");
-//		goodClient.setKeystorePassphrase("thestorepass");
-//		goodClient.setKeystoreType("JCEKS");
-//		Socket client = goodClient.createClientSocket();
-//		client.connect(new InetSocketAddress("localhost", myPort));
-//		
-//		client.getOutputStream().write("HELLO WORLD".getBytes());
-//		client.close();
-//		
-//		Thread.sleep(500);
-//		assertNotSame("HELLO WORLD", receiver.myString);
-		
-	}
 
 	@Test
 	public void testConnectToNonTrustedSocket() throws IOException, InterruptedException {
-		
-//		TlsSocketFactory badServer = new TlsSocketFactory();
-//		Receiver receiver = new Receiver(badServer);
-//		receiver.start();
-//		Thread.sleep(500);
-//		
-//		CustomCertificateTlsSocketFactory goodClient = new CustomCertificateTlsSocketFactory();
-//		goodClient.setKeystoreFilename("src/test/resources/symmetrickey.jks");
-//		goodClient.setKeystorePassphrase("thestorepass");
-//		goodClient.setKeystoreType("JCEKS");
-//		Socket client = goodClient.createClientSocket();
-//		client.setSoTimeout(1000);
-//		client.connect(new InetSocketAddress("localhost", myPort));
-//		
-//		client.getOutputStream().write("HELLO WORLD".getBytes());
-//		client.close();
-//		
-//		Thread.sleep(500);
-//		assertNotSame("HELLO WORLD", receiver.myString);
-		
+
+		CustomCertificateTlsSocketFactory badServer = new CustomCertificateTlsSocketFactory();
+		badServer.setKeystoreFilename("src/test/resources/keystore.jks");
+		badServer.setKeystorePassphrase("changeit");
+		Receiver receiver = new Receiver(badServer);
+		receiver.start();
+		Thread.sleep(500);
+
+		try {
+
+			CustomCertificateTlsSocketFactory goodClient = new CustomCertificateTlsSocketFactory();
+			goodClient.setKeystoreFilename("src/test/resources/truststore2.jks");
+			goodClient.setKeystorePassphrase("trustpassword");
+			Socket client = goodClient.createClientSocket();
+			client.connect(new InetSocketAddress("localhost", myPort));
+
+			client.getOutputStream().write("HELLO WORLD".getBytes());
+			fail();
+			
+		} catch (SSLHandshakeException e) {
+
+		}
 	}
 
-	
+
+	@Test
+	public void testConnectToTrustedSocket() throws IOException, InterruptedException {
+
+		CustomCertificateTlsSocketFactory badServer = new CustomCertificateTlsSocketFactory();
+		badServer.setKeystoreFilename("src/test/resources/keystore.jks");
+		badServer.setKeystorePassphrase("changeit");
+		Receiver receiver = new Receiver(badServer);
+		receiver.start();
+		Thread.sleep(500);
+
+		CustomCertificateTlsSocketFactory goodClient = new CustomCertificateTlsSocketFactory();
+		goodClient.setKeystoreFilename("src/test/resources/truststore.jks");
+		goodClient.setKeystorePassphrase("changeit");
+		Socket client = goodClient.createClientSocket();
+		client.connect(new InetSocketAddress("localhost", myPort));
+
+		client.getOutputStream().write("HELLO WORLD".getBytes());
+		client.close();
+
+		Thread.sleep(500);
+		String expected = "HELLO WORLD";
+		String actual = receiver.myString;
+		assertEquals(expected, actual);
+
+	}
+
 	private class Receiver extends Thread {
 
-		private ISocketFactory myFactory;
 		private Throwable myException;
+		private ISocketFactory myFactory;
 		private ServerSocket myServer;
 		private String myString;
+
 
 		public Receiver(ISocketFactory theFactory) {
 			myFactory = theFactory;
 		}
 
+
 		@Override
 		public void run() {
 			try {
 
+				ourLog.info("Listening on port {}", myPort);
+
 				myServer = myFactory.createServerSocket();
 				myServer.bind(new InetSocketAddress(myPort));
 				myServer.setSoTimeout(3000);
-				
-				SSLServerSocket ss = (SSLServerSocket)myServer;
+
+				SSLServerSocket ss = (SSLServerSocket) myServer;
 				ourLog.info(Arrays.asList(ss.getEnabledCipherSuites()).toString());
-				
+
 				Socket socket = myServer.accept();
 				socket.setSoTimeout(2000);
-				
+
 				InputStream is = socket.getInputStream();
 				StringBuilder b = new StringBuilder();
 				for (;;) {
@@ -109,7 +121,7 @@ public class CustomCertificateTlsSocketFactoryTest {
 					if (next == -1) {
 						break;
 					} else {
-						b.append((char)next);
+						b.append((char) next);
 						ourLog.info("Received: " + b);
 					}
 				}
@@ -131,5 +143,19 @@ public class CustomCertificateTlsSocketFactoryTest {
 
 	}
 
-	private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(CustomCertificateTlsSocketFactoryTest.class);
+
+	public static void main(String[] args) throws Exception {
+
+		Server s = new Server();
+
+		SslSelectChannelConnector ssl = new SslSelectChannelConnector();
+		ssl.setKeystore("src/test/resources/keystore.jks");
+		ssl.setPassword("changeit");
+		ssl.setKeyPassword("changeit");
+		ssl.setPort(60647);
+
+		s.addConnector(ssl);
+		s.start();
+	}
+
 }
