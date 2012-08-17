08ffea104e88e8fec1f092629413cb2ba3021a70
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-08-17 12:57:59 +0000

    Test for issue 3558962. Doesn't pass yet.

---
+++ trunk/hapi-mvn/hapi-test/src/test/java/ca/uhn/hl7v2/unittest/custommodel/v25/message/ZDT_A01.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.uhn.hl7v2.unittest.custommodel.v25.message;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.parser.ModelClassFactory;
+import ca.uhn.hl7v2.unittest.custommodel.v25.segment.ZFA;
+
+/**
+ * Example custom model class. This is a ADT^A01, which is an ADT^A01 with
+ * an extra ZPI segment after the PID segment
+ *
+ * Since we're extending an existing HL7 message type, we just extend from the model class
+ * representing that type
+ */
+public class ZDT_A01 extends ca.uhn.hl7v2.model.v25.message.ADT_A01 {
+
+ 	/**
+	 * Constructor
+     *
+     * We always have to have a constructor with this one argument
+	 */
+	public ZDT_A01(ModelClassFactory factory) throws HL7Exception {
+	   super(factory);
+
+	   this.add(ZFA.class, false, true);
+	}
+
+
+    /**
+     * Add an accessor for the ZFA segment
+     */
+    public ZFA getZFA() throws HL7Exception {
+        return (ZFA) get("ZFA");
+    }
+
+}
