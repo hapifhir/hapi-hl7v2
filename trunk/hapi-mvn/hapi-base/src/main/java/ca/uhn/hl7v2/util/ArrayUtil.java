13b1dad19a43a29ead66cf4f0450e2d84ddae2a6
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2012-12-06 16:04:23 +0000

    New utility class

---
+++ trunk/hapi-mvn/hapi-base/src/main/java/ca/uhn/hl7v2/util/ArrayUtil.java
@@ -0,0 +1,28 @@
+package ca.uhn.hl7v2.util;
+
+/**
+ * Utility methods for working with arrays
+ */
+public class ArrayUtil {
+
+	/**
+	 * Returns true if the array contains the given value, using
+	 * {@link Object#equals(Object)}. Checks for a null value
+	 * in the array if theValue is null.
+	 */
+	public static <T> boolean contains(T[] theArray, T theValue) {
+		for (T next : theArray) {
+			if (theValue == null) {
+				if (next == null) {
+					return true;
+				}
+			} else {
+				if (theValue.equals(next)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+}
