b7ce55dc7bfe6c4a6f2dc73df079f3e463f9cf08
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2013-04-24 13:53:39 +0000

    Fix 182- Crash when using CanonicalModelClassFactory with a hardcoded message ttype

--- trunk/hapi-mvn/hapi-testmindeps/src/test/java/ca/uhn/hl7v2/parser/CanonicalModelClassFactoryTest.java
+++ trunk/hapi-mvn/hapi-testmindeps/src/test/java/ca/uhn/hl7v2/parser/CanonicalModelClassFactoryTest.java
@@ -10,25 +10,6 @@ import ca.uhn.hl7v2.model.Message;
 
 public class CanonicalModelClassFactoryTest {
 
-	@Test
-	public void testAckVersion() throws EncodingNotSupportedException, HL7Exception, IOException {
-		
-		String messageText = "MSH|^~\\&|4265-ADT|4265|eReferral|eReferral|201004141020||ADT^A45^ADT_A45|102416|T^|2.5^^|||NE|AL|CAN|8859/1\r"
-				+ "EVN|A45|201004141020|\r"
-				+ "PID|1||7010226^^^4265^MR~0000000000^^^CANON^JHN^^^^^^GP~1736465^^^4265^VN||Park^Green^^^MS.^^L||19890812|F|||123 TestingLane^^TORONTO^CA-ON^M5G2C2^CAN^H^~^^^^^^^||^PRN^PH^^1^416^2525252^|^^^^^^^||||||||||||||||N\r"
-				+ "PV1|1|I||||^^^WP^1469^^^^^^^^|||||||||||^Derkach^Peter.^^^Dr.||20913000131|||||||||||||||||||||||||201004011340|201004141018";
-
-		CanonicalModelClassFactory mcf = new CanonicalModelClassFactory("2.6");
-		PipeParser parser = new PipeParser(mcf);
-		Message message = parser.parse(messageText);
-		
-		Assert.assertEquals(ca.uhn.hl7v2.model.v26.message.ADT_A45.class, message.getClass());
-		
-		Message ack = message.generateACK();
-		Assert.assertEquals(ca.uhn.hl7v2.model.v26.message.ACK.class, ack.getClass());
-		
-	}
-	
 	/**
 	 * https://sourceforge.net/p/hl7api/bugs/182/
 	 */
