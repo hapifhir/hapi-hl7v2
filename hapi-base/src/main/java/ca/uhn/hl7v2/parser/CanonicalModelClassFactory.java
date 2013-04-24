3917a588b995cd3fb4553e3c40f7b47d2421fa78
James Agnew <jamesagnew@gmail.com>
2013-04-24 13:53:39 +0000

    Fix 182- Crash when using CanonicalModelClassFactory with a hardcoded message ttype

--- hapi-base/src/main/java/ca/uhn/hl7v2/parser/CanonicalModelClassFactory.java
+++ hapi-base/src/main/java/ca/uhn/hl7v2/parser/CanonicalModelClassFactory.java
@@ -20,7 +20,7 @@ and replace  them with the notice and other provisions required by the GPL Licen
 If you do not delete the provisions above, a recipient may use your version of
 this file under either the MPL or the GPL.
 
-*/
+ */
 package ca.uhn.hl7v2.parser;
 
 import ca.uhn.hl7v2.HL7Exception;
@@ -30,91 +30,104 @@ import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.Segment;
 import ca.uhn.hl7v2.model.SuperStructure;
 import ca.uhn.hl7v2.model.Type;
+import ca.uhn.hl7v2.util.ReflectionUtil;
 
 /**
- * Specialized version of ModelClassFactory that always returns the same version or even structure. This is useful when designing
- * applications which are expected to handle multiple versions of HL7. The recommended approach is to
- * configure this factory to handle the newest version of HL7 you intend to support. Since HL7 is a backwards
- * compatible protocol, older versions should always be able to parse correctly into a newer message structure.  
+ * Specialized version of ModelClassFactory that always returns the same version
+ * or even structure. This is useful when designing applications which are
+ * expected to handle multiple versions of HL7. The recommended approach is to
+ * configure this factory to handle the newest version of HL7 you intend to
+ * support. Since HL7 is a backwards compatible protocol, older versions should
+ * always be able to parse correctly into a newer message structure.
  * 
- * @version $Revision: 1.2 $ updated on $Date: 2009-10-03 15:25:46 $ by $Author: jamesagnew $
- * @author This ModelClassFactory implementation is modified by Niranjan.Sharma@med.ge.com on 27-Jul-2009 for CanonicalModel of V2.6
+ * @version $Revision: 1.2 $ updated on $Date: 2009-10-03 15:25:46 $ by $Author:
+ *          jamesagnew $
+ * @author This ModelClassFactory implementation is modified by
+ *         Niranjan.Sharma@med.ge.com on 27-Jul-2009 for CanonicalModel of V2.6
  */
-public class CanonicalModelClassFactory extends DefaultModelClassFactory
-{
+public class CanonicalModelClassFactory extends DefaultModelClassFactory {
 
-    private static final long serialVersionUID = -1795680089524220526L;
-    
-    private String myVersion;
+	private static final long serialVersionUID = -1795680089524220526L;
 
 	private Class<? extends Message> myMessageClass;
 
-    /**
-     * Constructor which selects the newest version of HAPI known to
-     */
-    public CanonicalModelClassFactory() {
-        myVersion = getHighestKnownVersion();
-    }
+	private String myVersion;
 
-    /**
-     * Constructor
-     * 
-     * @param theVersion The version to always return (e.g. "2.6")
-     */
-    public CanonicalModelClassFactory(String theVersion) {
-    	if (theVersion == null || !Version.supportsVersion(theVersion)) {
-    		throw new IllegalArgumentException("Unknown version: " + theVersion);
-    	}
-        myVersion = theVersion;
-    }
-
-    /**
-     * Constructor for a model class factory which always returns the same
-     * message type.
-     * 
-     * @see SuperStructure
-     */
-    public CanonicalModelClassFactory(Class<? extends Message> theClass) {
-    	if (theClass == null) {
-    		throw new NullPointerException("Class may not be null");
-    	}
-    	myMessageClass = theClass;
-    }
-    
-    
 	/**
-     * {@inheritDoc}
-     */
-    @Override
-    public Class<? extends Group> getGroupClass(String theName, String theVersion) throws HL7Exception {
-        return super.getGroupClass(theName, myVersion);
-    }
+	 * Constructor which selects the newest version of HAPI known to
+	 */
+	public CanonicalModelClassFactory() {
+		myVersion = getHighestKnownVersion();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Class<? extends Message> getMessageClass(String theName, String theVersion, boolean theIsExplicit) throws HL7Exception {
-    	if (myMessageClass != null) {
-    		return myMessageClass;
-    	}
-        return super.getMessageClass(theName, myVersion, theIsExplicit);
-    }
+	/**
+	 * Constructor for a model class factory which always returns the same
+	 * message type.
+	 * 
+	 * @see SuperStructure
+	 */
+	public CanonicalModelClassFactory(Class<? extends Message> theClass) {
+		if (theClass == null) {
+			throw new NullPointerException("Class may not be null");
+		}
+		myMessageClass = theClass;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Class<? extends Segment> getSegmentClass(String theName, String theVersion) throws HL7Exception {
-        return super.getSegmentClass(theName, myVersion);
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param theVersion
+	 *            The version to always return (e.g. "2.6")
+	 */
+	public CanonicalModelClassFactory(String theVersion) {
+		if (theVersion == null || !Version.supportsVersion(theVersion)) {
+			throw new IllegalArgumentException("Unknown version: " + theVersion);
+		}
+		myVersion = theVersion;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Class<? extends Type> getTypeClass(String theName, String theVersion) throws HL7Exception {
-        return super.getTypeClass(theName, myVersion);
-    }
-    
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<? extends Group> getGroupClass(String theName, String theVersion) throws HL7Exception {
+		return super.getGroupClass(theName, myVersion);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<? extends Message> getMessageClass(String theName, String theVersion, boolean theIsExplicit) throws HL7Exception {
+		if (myMessageClass != null) {
+			return myMessageClass;
+		}
+		initVersionIfNeeded();
+		return super.getMessageClass(theName, myVersion, theIsExplicit);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<? extends Segment> getSegmentClass(String theName, String theVersion) throws HL7Exception {
+		initVersionIfNeeded();
+		return super.getSegmentClass(theName, myVersion);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<? extends Type> getTypeClass(String theName, String theVersion) throws HL7Exception {
+		initVersionIfNeeded();
+		return super.getTypeClass(theName, myVersion);
+	}
+
+	private void initVersionIfNeeded() throws HL7Exception {
+		if (myMessageClass != null && myVersion == null) {
+			myVersion = ReflectionUtil.instantiateMessage(myMessageClass, this).getVersion();
+		}
+	}
+
 }
