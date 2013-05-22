e7234194777dfbdbebef010679b0179a8d8d113b
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2013-05-22 21:31:49 +0000

    Better L&F for Linux

---
+++ trunk/hapi-mvn/hapi-testpanel/src/main/java/ca/uhn/hl7v2/testpanel/LinuxInitializer.java
@@ -0,0 +1,21 @@
+package ca.uhn.hl7v2.testpanel;
+
+import javax.swing.UIManager;
+
+import ca.uhn.hl7v2.testpanel.controller.Controller;
+
+public class LinuxInitializer {
+
+	public void run(final Controller theController) {
+		
+		try {
+			// Set System L&F
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+
+		
+	}
+
+}
