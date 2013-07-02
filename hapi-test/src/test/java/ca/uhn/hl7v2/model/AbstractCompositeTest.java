a6f90c0e3ecbc4c7ec7907dc158d9eee4b17cb54
James Agnew <jamesagnew@gmail.com>
2013-07-02 23:21:51 +0000

    Formatting

--- hapi-test/src/test/java/ca/uhn/hl7v2/model/AbstractCompositeTest.java
+++ hapi-test/src/test/java/ca/uhn/hl7v2/model/AbstractCompositeTest.java
@@ -1,29 +1,30 @@
 package ca.uhn.hl7v2.model;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
 import org.junit.Test;
 
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.v25.message.ORU_R01;
-import static org.junit.Assert.*;
 
 public class AbstractCompositeTest {
 
 	@Test
 	public void testParseNull() throws HL7Exception, IOException {
-		
+
 		ORU_R01 msg = new ORU_R01();
 		msg.initQuickstart("ORU", "R01", "T");
-		
+
 		assertTrue(msg.getMSH().getMessageType().encode().length() > 0);
-		
+
 		msg.getMSH().getMessageType().parse(null);
-		
+
 		assertNull(msg.getMSH().getMessageType().getMessageCode().getValue());
 		assertNull(msg.getMSH().getMessageType().getMessageStructure().getValue());
 
-		
 	}
-	
+
+
 }
