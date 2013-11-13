e5b408979193f71e182c73034da5b255bb387c37
jamesagnew <jamesagnew@23f2b886-cf13-4d23-8097-e996e248ff02>
2013-11-13 12:31:40 +0000

    Another script for building

---
+++ trunk/hapi-mvn/scripts/build_versions.sh
@@ -0,0 +1,10 @@
+mvn -P ACCESS,GEN generate-resources
+mvn install; cd ..
+cd hapi-structures-v22/; mvn install; cd ..
+cd hapi-structures-v23/; mvn install; cd ..
+cd hapi-structures-v231/; mvn install; cd ..
+cd hapi-structures-v24/; mvn install; cd ..
+cd hapi-structures-v25/; mvn install; cd ..
+cd hapi-structures-v251/; mvn install; cd ..
+cd hapi-structures-v26/; mvn install; cd ..
+cd hapi-structures-v231-alternatenames/; mvn install; cd ..
