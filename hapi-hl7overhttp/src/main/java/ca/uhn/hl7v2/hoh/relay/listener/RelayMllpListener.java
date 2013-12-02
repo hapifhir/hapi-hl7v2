22f2d19f171e64b70fb199613457ef50e82ac142
James Agnew <jamesagnew@gmail.com>
2013-12-02 19:37:30 +0000

    Improve thread names in the HoH relay worker threads

--- hapi-hl7overhttp/src/main/java/ca/uhn/hl7v2/hoh/relay/listener/RelayMllpListener.java
+++ hapi-hl7overhttp/src/main/java/ca/uhn/hl7v2/hoh/relay/listener/RelayMllpListener.java
@@ -2,18 +2,19 @@ package ca.uhn.hl7v2.hoh.relay.listener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.app.SimpleServer;
 import ca.uhn.hl7v2.hoh.util.Validate;
-import ca.uhn.hl7v2.llp.ExtendedMinLowerLayerProtocol;
-import ca.uhn.hl7v2.llp.LowerLayerProtocol;
+import ca.uhn.hl7v2.llp.MinLowerLayerProtocol;
 import ca.uhn.hl7v2.parser.GenericModelClassFactory;
-import ca.uhn.hl7v2.parser.Parser;
-import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.protocol.ApplicationRouter.AppRoutingData;
 import ca.uhn.hl7v2.protocol.ReceivingApplication;
 
@@ -25,6 +26,7 @@ public class RelayMllpListener implements InitializingBean, DisposableBean, IRel
 	private String myBeanName;
 	private int myPort;
 	private SimpleServer myServer;
+	private AtomicInteger threadNum	= new AtomicInteger(1);
 
 	/**
 	 * Fired automatically by the container when
@@ -35,9 +37,11 @@ public class RelayMllpListener implements InitializingBean, DisposableBean, IRel
 			throw new IllegalStateException("Port not set");
 		}
 		
-		LowerLayerProtocol llp = new ExtendedMinLowerLayerProtocol();
-		Parser parser = new PipeParser(new GenericModelClassFactory());
-		myServer = new SimpleServer(myPort, llp, parser);
+		DefaultHapiContext ctx = new DefaultHapiContext();
+		ctx.setExecutorService(Executors.newCachedThreadPool(new MyThreadFactory()));
+		ctx.setLowerLayerProtocol(new MinLowerLayerProtocol(true));
+		ctx.setModelClassFactory(new GenericModelClassFactory());
+		myServer = ctx.newServer(myPort, false);
 
 		for (int i = 0; i < myAppRoutingData.size(); i++) {
 			myServer.registerApplication(myAppRoutingData.get(i), myApplications.get(i));
@@ -94,6 +98,28 @@ public class RelayMllpListener implements InitializingBean, DisposableBean, IRel
 
 	public void setPort(int thePort) {
 		myPort = thePort;
+	}
+
+	private class MyThreadFactory implements ThreadFactory {
+
+		private ThreadGroup group;
+
+		private MyThreadFactory() {
+			group = Thread.currentThread().getThreadGroup();
+		}
+
+		public Thread newThread(Runnable theR) {
+			String name = "hoh-port-" + myPort + "-worker-" + threadNum.getAndIncrement();
+			Thread t = new Thread(group, theR, name, 0);
+			if (t.isDaemon()) {
+				t.setDaemon(false);
+			}
+			if (t.getPriority() != Thread.NORM_PRIORITY) {
+				t.setPriority(Thread.NORM_PRIORITY);
+			}
+			return t;
+		}
+
 	}
 
 
