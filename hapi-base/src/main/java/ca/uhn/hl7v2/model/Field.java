63df906d8f9a03ada438284f405d41c7d8a5dfa6
Amy Wilson <amy.wilson@comrad.co.nz>
2020-05-08 10:56:07 +1200

    INT-178 MessageVisitor does not understand repeating fields properly. This commit is a combination of two commits made on mourezwell's fork of hapi-hl7v2 (https://github.com/mourezwell/hapi-hl7v2) https://github.com/mourezwell/hapi-hl7v2/commit/935bb334ee3017b9795253b00e795816d90ce565 Fix Field repetition in Location object when required https://github.com/mourezwell/hapi-hl7v2/commit/d68104a958e84d74e6a3402bfe4be0ac8a57d00a Continue reading after empty

--- hapi-base/src/main/java/ca/uhn/hl7v2/model/Field.java
+++ hapi-base/src/main/java/ca/uhn/hl7v2/model/Field.java
@@ -5,15 +5,15 @@
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
  * specific language governing rights and limitations under the License.
- *
+ * <p>
  * The Original Code is "Field.java".  Description:
  * Auxilliary model class encapsulating field repetitions and substructures
- *
+ * <p>
  * The Initial Developer of the Original Code is University Health Network. Copyright (C)
  * 2001-2013.  All Rights Reserved.
- *
+ * <p>
  * Contributor(s): ______________________________________.
- *
+ * <p>
  * Alternatively, the contents of this file may be used under the terms of the
  * GNU General Public License (the  �GPL�), in which case the provisions of the GPL are
  * applicable instead of those above.  If you wish to allow use of your version of this
@@ -22,7 +22,6 @@
  * and replace  them with the notice and other provisions required by the GPL License.
  * If you do not delete the provisions above, a recipient may use your version of
  * this file under either the MPL or the GPL.
- *
  */
 package ca.uhn.hl7v2.model;
 
@@ -37,29 +36,36 @@ public class Field implements Visitable {
 
     private Type[] reps;
     private int maxCardinality;
-    
-    
+
     public Field(Type[] reps, int maxCardinality) {
         super();
         this.reps = reps;
         this.maxCardinality = maxCardinality;
     }
 
+    @Override
     public boolean accept(MessageVisitor visitor, Location currentLocation) throws HL7Exception {
         if (visitor.start(this, currentLocation)) {
-            for (int i=0; i < reps.length; i++) {
+            for (int i = 0; i < reps.length; i++) {
                 Type t = reps[i];
-                if (!t.accept(visitor, currentLocation))
+                Location nextLocation = currentLocation;
+                if (reps.length > 1) {
+                    nextLocation = provideLocation(currentLocation, currentLocation.getField(), i);
+                }
+                if (!t.accept(visitor, nextLocation)) {
                     break;
+                }
             }
         }
         return visitor.end(this, currentLocation);
     }
 
+    @Override
     public Location provideLocation(Location parentLocation, int index, int repetition) {
-        return new Location(parentLocation).withField(index);
+        return new Location(parentLocation).withField(index).withFieldRepetition(repetition);
     }
 
+    @Override
     public boolean isEmpty() throws HL7Exception {
         return reps.length == 0;
     }
--- hapi-test/src/test/java/ca/uhn/hl7v2/model/MessageVisitorTest.java
+++ hapi-test/src/test/java/ca/uhn/hl7v2/model/MessageVisitorTest.java
@@ -1,9 +1,9 @@
 package ca.uhn.hl7v2.model;
 
-
 import static ca.uhn.hl7v2.model.MessageVisitors.visit;
 import static ca.uhn.hl7v2.model.MessageVisitors.visitPopulatedElements;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,13 +12,14 @@ import java.io.InputStreamReader;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-// import ca.uhn.hl7v2.parser.PipeEncoder;
 import org.junit.Test;
 
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.Location;
 import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.util.Terser;
+
+// import ca.uhn.hl7v2.parser.PipeEncoder;
 
 public class MessageVisitorTest {
 
@@ -27,13 +28,13 @@ public class MessageVisitorTest {
         String msg = loadFile("/ca/uhn/hl7v2/parser/omd_o03.txt", '\r');
         PipeParser pipeParser = new PipeParser();
         Message message = pipeParser.parse(msg);
-        
+
         // Visit all non-empty elements using the TestMessageVisitor
         TestMessageVisitor tmv = visit(message, visitPopulatedElements(new TestMessageVisitor())).getDelegate();
-        
+
         assertEquals("Visitor start/end method calls not equal", 0, tmv.level);
-        assertEquals("Wrong number of non-empty primitive values", 44, tmv.nonEmptyPrimitives.size());
-        
+        assertEquals("Wrong number of non-empty primitive values", 58, tmv.nonEmptyPrimitives.size());
+
         // Test that all non-empty primitive locations can be used as input to Terser.get(String)
         // and all returned values correspond with the visited values.
         Terser terser = new Terser(message);
@@ -41,25 +42,56 @@ public class MessageVisitorTest {
             assertEquals("Unexpected value", terser.get(l.toString()), tmv.nonEmptyPrimitives.get(l));
         }
     }
-/**
+
     @Test
-    public void testEncodeOmdO03() throws HL7Exception, IOException {
-        String msg = loadFile("/ca/uhn/hl7v2/parser/omd_o03.txt", '\r');
+    public void testVisitOmdO03WithRep() throws HL7Exception, IOException {
+        String msg = loadFile("/ca/uhn/hl7v2/parser/omd_o03_rep.txt", '\r');
         PipeParser pipeParser = new PipeParser();
         Message message = pipeParser.parse(msg);
 
         // Visit all non-empty elements using the TestMessageVisitor
-        PipeEncoder ppe = visit(message, new PipeEncoder(pipeParser.getParserConfiguration()));
-        System.out.println(ppe.getEncoded());
+        TestMessageVisitor tmv = visit(message, visitPopulatedElements(new TestMessageVisitor())).getDelegate();
+
+        assertEquals("Visitor start/end method calls not equal", 0, tmv.level);
+        assertEquals("Wrong number of non-empty primitive values", 61, tmv.nonEmptyPrimitives.size());
+
+        // PID|1||CIPNUM^^^CAEX^CIP^^^^EX&&ES-EX~CIPTWO||DUMAS^VICTOR HUGO|ZAPATA|19740325|1||||||||||||||||||||||||A~B
+
+        // Test that all non-empty primitive locations can be used as input to Terser.get(String)
+        // and all returned values correspond with the visited values.
+        Terser terser = new Terser(message);
+
+        assertEquals("CIPNUM", terser.get("/PATIENT/PID-3(0)-1"));
+        assertEquals("CIPTWO", terser.get("/PATIENT/PID-3(1)-1"));
+        assertEquals("CIPNUM", terser.get("/PATIENT/PID-3-1"));
+        assertEquals("A", terser.get("/PATIENT/PID-32(0)"));
+        assertEquals("B", terser.get("/PATIENT/PID-32(1)"));
+        assertEquals("A", terser.get("/PATIENT/PID-32"));
+        assertEquals("DUMAS", terser.get("/PATIENT/PID-5(0)-1"));
+        assertNull(terser.get("/PATIENT/PID-6(1)-1"));
+
+        for (Location l : tmv.nonEmptyPrimitives.keySet()) {
+            assertEquals("Unexpected value for " + l.toString(), terser.get(l.toString()), tmv.nonEmptyPrimitives.get(l));
+        }
     }
-*/
-    
+    /**
+     @Test public void testEncodeOmdO03() throws HL7Exception, IOException {
+     String msg = loadFile("/ca/uhn/hl7v2/parser/omd_o03.txt", '\r');
+     PipeParser pipeParser = new PipeParser();
+     Message message = pipeParser.parse(msg);
+
+     // Visit all non-empty elements using the TestMessageVisitor
+     PipeEncoder ppe = visit(message, new PipeEncoder(pipeParser.getParserConfiguration()));
+     System.out.println(ppe.getEncoded());
+     }
+     */
+
     /**
      * Visitor that counts start/end method calls and remembers all non-empty
      * primitive values associated with their respective location.
      */
     private class TestMessageVisitor extends MessageVisitorSupport {
-        
+
         Map<Location, String> nonEmptyPrimitives = new LinkedHashMap<Location, String>();
         int level = 0;
 
